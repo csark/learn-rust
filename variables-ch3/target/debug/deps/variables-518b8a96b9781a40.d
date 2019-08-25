7c93bdf5de93cd80e054ef17c53040e66b511818
Clark <c.washington.h@gmail.com>
2019-08-24 22:23:12 -0700

    Chapter 3 initial commit

---
+++ variables-ch3/target/debug/deps/variables-518b8a96b9781a40.d
@@ -0,0 +1,5 @@
+/home/clark/rust/learn-rust/variables/target/debug/deps/variables-518b8a96b9781a40: src/main.rs
+
+/home/clark/rust/learn-rust/variables/target/debug/deps/variables-518b8a96b9781a40.d: src/main.rs
+
+src/main.rs:
---
+++ variables-ch3/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/clark/rust/learn-rust/variables/target/debug/variables: /home/clark/rust/learn-rust/variables/src/main.rs
