8ccc4cdd23ce581964cbe7c8b4b15697a919b90e
Clark <c.washington.h@outlook.com>
2019-09-02 21:47:29 -0700

    Adding last section of chapter 3

---
+++ ch3-branches/src/main.rs
@@ -0,0 +1,86 @@
+// fn main() {
+//     let number = 6;
+//
+//     if number % 4 == 0 {
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         println!("number is divisible by 3");
+//     } else if number % 2 == 0 {
+//         println!("number is divisible by 2");
+//     } else {
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
+// // Because `if` is an expression it can be used on the right side
+// // of a let statement
+// fn main() {
+//     let condition = true;
+//     let number = if condition {
+//         5
+//     } else {
+//         6
+//     };
+//
+//     println!("The value of number is: {}", number);
+// }
+
+// // This will not compile as there mismatched variables types in the arms
+// // of the conditional statement.
+// // RUST NEEDS TO KNOW AT COMPILE TIME WHAT TYPE THE NUMBER VARIABLE IS
+// fn main() {
+//     let condition = true;
+//
+//     let number = if condition {
+//         5
+//     } else {
+//         "six"
+//     };
+//
+//     println!("The value of number is: {}", number);
+// }
+
+// // loop
+// fn main() {
+//     let mut counter = 0;
+//
+//     let result = loop {
+//         counter += 1;
+//
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+//     };
+//
+//     println!("The result is {}", result);
+// }
+
+// // while
+// fn main() {
+//     let mut number = 3;
+//
+//     while number != 0 {
+//         println!("{}!", number);
+//
+//         number -= 1;
+//     }
+//
+//     println!("LIFTOFF!!!");
+// }
+
+// // for
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+//
+//     for element in a.iter() {
+//         println!("the value is: {}", element);
+//     }
+// }
+
+// for loop using range and .rev to reverse the range
+fn main() {
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
