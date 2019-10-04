2b0810cf3efc6f3ad7f39e3e379dc1bab16d98c2
Clark Hatch <chatch@pinterest.com>
2019-10-04 15:53:44 -0700

    Adding chapter 8 work

---
+++ ch8-vectors/src/main.rs
@@ -0,0 +1,54 @@
+#![allow(unused_variables)]
+fn main() {
+    // let v: Vec<i32> = Vec::new();
+    // let v = vec![1,2,3]
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("The third element is {}", third),
+    }
+    // Bad Mutable/Immutable references, This will make the compiler angry
+    // let w = vec![1,2,3,4,5];
+    // let first = &v[0];
+    //
+    // v.push(6);
+    //
+    // println!("The first element is: {}", first);
+    // End angry compiler
+
+    // Loop over a vector
+    let a = vec![100,32,57];
+    for i in &a {
+        println!("{}", i);
+    }
+
+    // Loop over a vector with ability to change contents
+    let mut b = vec![100,32,57];
+    for i in &mut b {
+        println!("Old value i: {}", i);
+        *i += 50;
+        println!("New value i: {}", i);
+    }
+
+    // Using an enum to store multiple types
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
