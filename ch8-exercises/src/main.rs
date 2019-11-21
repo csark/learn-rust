d5099776eae470312950f6d83a7ce94a05d6aaf0
Clark <mail@ofclark.com>
2019-11-20 22:56:55 -0800

    Exercises

---
+++ ch8-exercises/src/main.rs
@@ -0,0 +1,36 @@
+fn mean(list: &[i32]) -> f32 {
+    let mut total = 0;
+    for i in list {
+        total += i;
+    }
+
+    (total as f32)/(list.len() as f32)
+}
+
+fn median(list: &[i32]) -> i32 {
+    let example = 4;
+    example
+}
+
+fn mode(list: &[i32]) -> i32 {
+    let example = 4;
+    example
+}
+
+fn main() {
+    let mut v = Vec::new();
+
+    v.push(1);
+    v.push(2);
+    v.push(3);
+    v.push(4);
+    v.push(5);
+    v.push(4);
+    v.push(5);
+    v.push(3);
+    v.push(2);
+    v.push(1);
+    v.push(4);
+
+    println!("The mean is: {:.2}", mean(&v));
+}
