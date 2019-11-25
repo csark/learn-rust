612dd9f9d5a592a1818387e1e5f9cb7eb5d9fb7e
Clark <mail@ofclark.com>
2019-11-24 20:59:06 -0800

    Updated exercises file

--- ch8-exercises/src/main.rs
+++ ch8-exercises/src/main.rs
@@ -1,17 +1,27 @@
 use std::collections::HashMap;
 
-fn mean(list: &[i32]) -> f32 {
+fn mean(list: &[i32]) -> f64 {
     let mut total = 0;
     for i in list {
         total += i;
     }
 
-    (total as f32)/(list.len() as f32)
+    (total as f64)/(list.len() as f64)
 }
 
-fn median(list: &[i32]) -> i32 {
-    let mid: i32 = (list.len() as i32) / 2;
-    list[mid as usize]
+fn median(list: &[i32]) -> f64 {
+    // Original attempt by me (which works)
+    // let mid: i32 = (list.len() as i32) / 2;
+    // list[mid as usize]
+
+    // Answer from someone online
+    let len = list.len();
+    let mid = len / 2;
+    if len % 2 == 0 {
+        mean(&list[(mid - 1)..(mid + 1)])
+    } else {
+        f64::from(list[mid])
+    }
 }
 
 fn mode(list: &[i32]) -> i32 {
@@ -35,19 +45,19 @@ fn main() {
 
     v.push(1);
     v.push(2);
-    v.push(3);
+    // v.push(3);
     v.push(4);
     v.push(5);
     v.push(4);
     v.push(5);
-    v.push(3);
-    v.push(2);
+    v.push(5);
+    v.push(5);
     v.push(1);
     v.push(4);
 
     println!("The mean is: {:.2}", mean(&v));
     v.sort();
     // println!("{:?}", v);
-    println!("The median is: {:?}", median(&v));
+    println!("The median is: {:.2}", median(&v));
     println!("The mode is: {}", mode(&v));
 }
