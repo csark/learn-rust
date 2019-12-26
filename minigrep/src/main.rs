cfda7636051b2e936f71a78874351eb54ca41390
Clark Hatch <chatch@pinterest.com>
2019-12-26 11:24:49 -0700

    adding stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,14 +7,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     // if let is used here instead of unwrap_or_else because the run function
     // does not return a value, only ()
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 
